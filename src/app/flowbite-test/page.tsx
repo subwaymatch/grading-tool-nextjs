f3648d3427576ed8476ebc00793d38b0e89faa94
Ye Joo Park <subwaymatch@gmail.com>
2023-11-03 10:40:32 -0500

    Setup Nextjs 14 with typescript, tailwind, flowbite-react

---
+++ src/app/flowbite-test/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { Alert } from "flowbite-react";
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Flowbite Test</h1>
+      <Alert color="info">Alert!</Alert>
+    </div>
+  );
+}
